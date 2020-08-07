a5abf377ef347152d3bc43c955b0cdf49adcf427
Kseniia Ryuma <kseniya0213@gmail.com>
2020-08-07 02:32:03 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -22,6 +22,6 @@ output "aws_sub_zone_nameservers" {
 #   value = google_dns_managed_zone.gcp_sub_zone.*.name
 # }
 
-output "gcp_dns_zone_nameservers" {
-  value = google_dns_managed_zone.gcp_sub_zone.*.name_servers
-}
+# output "gcp_dns_zone_nameservers" {
+#   value = google_dns_managed_zone.gcp_sub_zone.*.name_servers
+# }
